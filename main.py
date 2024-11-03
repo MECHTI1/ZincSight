a395f7fc829e3f85a71fc48c7f7b67dd9bdd74c7
mechti <mechtinger1@mail.tau.ac.il>
2024-11-03 03:33:13 +0200

    * Completed main.py V1.0 * ign refined * Completed ZincSight.ipynb V1.0

--- main.py
+++ main.py
@@ -27,13 +27,10 @@ import os
 import sys
 import re
 from src.download_query_structures import download_query_structures
-from src.download_query_structures.aria2c_downloader import mount_storage
-from src.download_query_structures import aria2c_downloader
 from Bio.PDB import PDBParser, MMCIFIO
 from src.settings import QUERY_STRUCTURES_DIR
 from src.primary_script import main as execute
-import requests
-from dotenv import load_dotenv
+
 
 
 def str_clean_parse_tolist(input_string):
@@ -41,18 +38,17 @@ def str_clean_parse_tolist(input_string):
     clean_items_list = [item.strip() for item in cleaned_string.split(",") if item.strip()]  # Split by commas, strip leading/trailing spaces, and remove any empty strings
     return clean_items_list
 
+# TODO: need to check if I already implemented 'ESM' and 'PDB' options
 def split_struct_db_sources(list_non_processed_input_names):
     af_list, pdb_list, esm_list = [], [], []
     for id_name in list_non_processed_input_names:
-        if 'AF-' in id_name: # Insert AF uniprot ID into af_list
-            af_list.append(id_name.split('-')[1])  # Taking only the uniprot ID if bring full name
-        elif id_name.startswith("MGY"): # Insert to esm_list
-            esm_list.append(id_name)
-            print("need to add this 'ESM' option and 'PDB'")
-        elif len(id_name) == 4 and id_name[0].isdigit(): # Insert to pdb_list
-            pdb_list.append(id_name)
-            print("need to add this 'ESM' option and 'PDB'")
-        else:  # AF uniprot ID into af_list. If an item name not satisfied the above, function consider it as UniProt model of AF model
+        if 'AF-' in id_name:
+            af_list.append(id_name.split('-')[1])  # Append the af uniprot id to af_list
+        elif id_name.startswith("MGY"):
+            esm_list.append(id_name)  # Append full mgnify protein id to esm_list
+        elif len(id_name) == 4 and id_name[0].isdigit(): #4 letter name with a number in the start considered a PDB structure
+            pdb_list.append(id_name) # Insert to pdb_list
+        else:  # If an input id name is not starting with 'MGY' or is a 4 letter name (with a number in the start), it will be considered as s uniprot af model
             af_list.append(id_name)
     return af_list, pdb_list, esm_list
 
@@ -64,8 +60,10 @@ def primary_download_structures_list_input(string_of_ids_to_download):
         #print(af_list, pdb_list, esm_list)
         download_query_structures.main(af_list, pdb_list, esm_list)
 
-# Convert all PDB files in the provided directory and subdirectories to CIF format.
-def convert_all_pdb_in_dir(directory):
+
+
+def convert_all_pdb_to_cif_in_dir(directory):
+    """Convert all PDB files in the provided directory and subdirectories to CIF format"""
     # Helper function to convert PDB to CIF
     def convert_pdb_to_cif(pdb_file_path):
         try:
@@ -89,118 +87,33 @@ def convert_all_pdb_in_dir(directory):
                 convert_pdb_to_cif(pdb_file_path)
 
 
-if __name__ == "__main__":
-    #FIXME Maybe will use somewhere in this script:
-    # Checking_algo_with_default_input = True
+
+def execute_zincsight(boolean_his_rot):
     try:
-        #TODO: Delete from this script, but maybe maintain in an optional test module:
-        # ---------------------------------------------------------------------------
-        # print("For downloading structures press: 'd'. To upload structures: 'u'. For both options press '2'")
-        # pressed_choice = input()
-        # if pressed_choice == "2":
-        #     option_downloading = True
-        #     option_uploading = True
-        # elif pressed_choice == "u":
-        #     option_downloading = False
-        #     option_uploading = True
-        # elif pressed_choice == "d":
-        #     option_downloading = True
-        #     option_uploading = False
-        # ------------------------------------------------------------------------------
+        def get_user_uploaded_ids_if_exists(parent_dir_path, file_name):
+            file_path = os.path.join(parent_dir_path, file_name)
+            # Check if folder exists, is not empty, and file exists with non-empty content
+            if os.path.isfile(file_path):
+                with open(file_path, 'r') as f:
+                    content = f.read().strip()
+                    return content if content else False
+            return False
 
-        #TODO: Delete from this script, but maybe maintain in the notebook or also within an optional test module:
-        # ---------------------------------------------------------------------------
-        # if option_downloading:
-        #     print(""" Option 1:
-        #                 Please write a list of the protein structure identifiers- 'PDB','AF model' or 'ESMFold models'.
-        #                 For AF models- write a list of UniProt identifiers.
-        #                 For ESMFold models- write a list of their MGnify identifiers
-        #               Option 2:
-        #                 Upload a txt file list of structures download urls within local
-        #               Press 1 or 2 according your preference""")
-        # ---------------------------------------------------------------------------
+        str_struct_ids_for_download = get_user_uploaded_ids_if_exists('text_structures_ids_to_download', 'structure_ids_input.txt')
+        if str_struct_ids_for_download:
+            # Processes and downloads structure files based on input identifiers (AlphaFold/PDB/ESM formats)
+            primary_download_structures_list_input(str_struct_ids_for_download)
 
-
-            preference_list1_or_linkstxt2 = input()
-
-            if int(preference_list1_or_linkstxt2) == 1:
-                if Checking_algo_with_default_input:
-                    string_of_ids_to_download = """A0A068N621, A0A0F6AZI6, A0A292DHH8, A0A2U3D0N8, A0A3F2YM30,
-                                               A0A5H1ZR49, G8ZFK7, O60232, P0A6G5, P0DUH5, P37659, P38164,
-                                               Q03760, Q08281, Q2K0Z2, Q2UFA9, Q5W0Q7, Q66K64, Q68EN5,
-                                               Q6CXX6, Q7MVV4, Q86T03, Q8N8R7, Q8NBJ9, Q96JC1, Q9BWG6,
-                                               Q9D1N4, Q9KP27, Q9M1V3, Q9NUN7, Q9NXF7"""
-                else:
-                    string_of_ids_to_download = input()
-
-                primary_download_structures_list_input(string_of_ids_to_download)
-
-            elif int(preference_list1_or_linkstxt2) == 2:
-
-                # Add the urls txt file
-                if Checking_algo_with_default_input:
-                    # Testing_with_default_input
-                    url = "https://raw.githubusercontent.com/facebookresearch/esm/main/scripts/atlas/v0/full/tarballs.txt"  # GitHub raw file URL
-                    response = requests.get(url)
-                    directory_structures_urls_to_download = "src/download_query_structures/structures_urls_to_download.txt"
-                    with open(directory_structures_urls_to_download, "w") as file:
-                        file.write("\n".join(response.text.splitlines()[
-                                             :3]))  # Not using all links since then time check process would be time consuming
-
-                else:
-                    pass
-                    # TODO: add the colab box to upload the txt file
-
-                # Download structures to target directory and check if finish downloads with aria2c status to continue after
-                load_dotenv()  # Load environment variables from the .env file
-                if os.getenv('USER') == "mechti":
-                    temp_download_directory = str(os.getenv('LOCAL_STORAGE_DIRECTORY'))
-                    print(f"Using local storage directory: {temp_download_directory}")
-                else:
-                    print("Please choose a storage method for mounting: Google Drive or GCS.")
-                    service = input()
-                    # Get the mounted directory from the mount_storage function
-                    MOUNT_POINT = mount_storage(service)
-
-                    if MOUNT_POINT:
-                        temp_download_directory = os.path.join(MOUNT_POINT,
-                                                               'downloads_directory')  # Set the download directory based on the mounted directory
-                        os.makedirs(temp_download_directory,
-                                    exist_ok=True)  # Create the download directory if it doesn't exist
-                    else:
-                        print("No storage was mounted. Exiting.")
-                        exit(1)
-                target_directory = QUERY_STRUCTURES_DIR
-
-                aria2c_status = aria2c_downloader.main(directory_structures_urls_to_download, temp_download_directory)
-
-        if option_uploading:  # need to add interface uploading, when browse and select structure files
-            pass
-            # TODO: add the colab box to upload the structures and need to mount drive or google cloud for tracking process
-
-        convert_all_pdb_in_dir(QUERY_STRUCTURES_DIR)
+        # convert PDB formatted query structures to mmCIF format
+        convert_all_pdb_to_cif_in_dir(QUERY_STRUCTURES_DIR)
 
         list_query_structures_files_paths = []
         for root, dirs, files in os.walk(QUERY_STRUCTURES_DIR):
-            for filename in files:
-                print(os.path.join(QUERY_STRUCTURES_DIR, filename))
-                list_query_structures_files_paths.append(os.path.join(QUERY_STRUCTURES_DIR, filename))
+          for filename in files:
+               list_query_structures_files_paths.append(os.path.join(QUERY_STRUCTURES_DIR, filename))
+        execute(list_query_structures_files_paths, boolean_his_rot)
 
     except Exception as e:
-        print(f"An exception occurred: {e}\nPlease fill all the parameters.")
+        print(f"An exception occurred: {e}\n Please fill all the parameters.")
         sys.exit()
 
-    boolean_rotamer_examination = False  # initialize variable to False
-    while True:
-        print("Press N/Y for rotamer examination: ")
-        user_input = input().upper()
-        if user_input == "Y":
-            boolean_rotamer_examination = True
-            break
-        elif user_input == "N":
-            boolean_rotamer_examination = False
-            break
-        else:
-            print("Invalid input. Please press N or Y.")
-
-    execute(list_query_structures_files_paths, boolean_rotamer_examination)
