c9fe9a8321a51c4691485f2424bc37db62b96b6b
mechti <mechtinger1@mail.tau.ac.il>
2024-12-17 19:14:21 +0200

    * Add option for execute many structures written in many file formats * print total running time

--- main_execute.py
+++ main_execute.py
@@ -100,8 +100,10 @@ def execute_zincsight(boolean_his_rot, structure_ids_for_download, path_query_st
     return compressed_results_path
 
 if __name__=="__main__": #Behave like a test
+    import time
+    zincsight_start_execution = time.time()
+    
     from src.settings import QUERY_STRUCTURES_DIR, RESULTS_DIR
-
     path_query_structures = QUERY_STRUCTURES_DIR
     path_output= RESULTS_DIR
 
@@ -112,26 +114,37 @@ if __name__=="__main__": #Behave like a test
             b) Define path of txt file with structure ids 
         2) Upload structures
         """
+    manually_written_structure_ids_for_download = ""
+    structure_ids_from_txt_file= ""
 
-    # Testing - manually written structure ids
-    manually_written_structure_ids_for_download = """A0A068N621, A0A0F6AZI6, A0A292DHH8, A0A2U3D0N8, A0A3F2YM30,
-                                           A0A5H1ZR49, G8ZFK7, O60232, P0A6G5, P0DUH5, P37659, P38164,
-                                           Q03760, Q08281, Q2K0Z2, Q2UFA9, Q5W0Q7, Q66K64, Q68EN5,
-                                           Q6CXX6, Q7MVV4, Q86T03, Q8N8R7, Q8NBJ9, Q96JC1, Q9BWG6,
-                                           Q9D1N4, Q9KP27, Q9M1V3, Q9NUN7, Q9NXF7"""
+    d_manual_ids, d_path_file_ids, u_structures =[False,True,False]
 
-    # Testing - defined path of txt file include structure ids
-    path_file_with_structure_ids = os.path.join("Query_structures_ids_txt_file","structures_ids_to_download.txt")
+    if d_manual_ids or d_path_file_ids:
+        if d_manual_ids:  # Testing - manually written structure ids
+            manually_written_structure_ids_for_download = """A0A068N621, A0A0F6AZI6, A0A292DHH8, A0A2U3D0N8, A0A3F2YM30, A0A5H1ZR49, 
+            G8ZFK7, O60232, P0A6G5, P0DUH5, P37659, P38164,Q03760, Q08281, Q2K0Z2, Q2UFA9, Q5W0Q7, Q66K64, Q68EN5, Q6CXX6, Q7MVV4, 
+            Q86T03, Q8N8R7, Q8NBJ9, Q96JC1, Q9BWG6, Q9D1N4, Q9KP27, Q9M1V3, Q9NUN7, Q9NXF7"""
+        if d_path_file_ids:  # Testing - defined path of txt file include structure ids
+            # Option 1 for input:
+            path_file_with_structure_ids = os.path.join("Query_structures_ids_txt_file","structures_ids_to_download.txt")
 
-    # Read the file and convert its content to a comma-separated string
-    with open(path_file_with_structure_ids, "r") as file:
-        lines = file.read().splitlines()  # Read all lines and remove newline characters
-    structure_ids_from_txt_file = str(",".join(lines))   # Convert the list of lines to a comma-separated string
+            #Option 2 for input:
+            #path_file_with_structure_ids = os.path.join("Query_structures_ids_txt_file", "2-D- UniprotID_of_RepClusters_nMem70Plus.csv")
+
+            # Read the file and convert its content to a comma-separated string
+            with open(path_file_with_structure_ids, "r") as file:
+                lines = file.read().splitlines()  # Read all lines and remove newline characters
+            structure_ids_from_txt_file = str(",".join(lines))   # Convert the list of lines to a comma-separated string
+
 
     # Combine the raw manual and file-based IDs (Create a combined string with these two strings)
     structure_ids_for_download = manually_written_structure_ids_for_download + structure_ids_from_txt_file
-    boolean_his_rot = True
 
+
+    boolean_his_rot = True
     compressed_results_path = execute_zincsight(boolean_his_rot,structure_ids_for_download, path_query_structures,path_output)
     print (compressed_results_path)
+    
+    zincsight_total_execution_time = time.time()-zincsight_start_execution
+    print (zincsight_total_execution_time)
 
