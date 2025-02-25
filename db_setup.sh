28eb554db49e26911d920dc8e20e9a8f992bf836
Gilad Mechtinger <117726454+MECHTI1@users.noreply.github.com>
2025-02-25 18:00:05 +0200

    Update db_setup.sh

--- db_setup.sh
+++ db_setup.sh
@@ -1,3 +1,4 @@
 psql -U postgres -tc "SELECT 1 FROM pg_database WHERE datname = 'zincsight_pipeline_db';" | grep -q 1 || psql -U postgres -c "CREATE DATABASE zincsight_pipeline_db;"
 psql -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE zincsight_pipeline_db TO postgres;"
 psql -U postgres -c "ALTER USER postgres WITH PASSWORD 'postgres';"
+psql -U postgres -d zincsight_pipeline_db -P pager=off -f "src/setup_pg_db_with_tables/PostgreSQL_4_necessary_tables.sql"
