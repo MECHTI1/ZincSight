44a99f47336390fe78b49437047fafd2bf4ffa97
mechti <mechtinger1@mail.tau.ac.il>
2025-04-06 16:25:40 +0300

    setting.py changed to KEEP_TEMP_TABLES = True, for debugging

--- src/settings.py
+++ src/settings.py
@@ -10,6 +10,8 @@ import os
 import psycopg2
 from dotenv import load_dotenv
 
+
+
 # Set the base directory to the absolute path of the project directory
 base_directory =os.path.abspath(os.path.dirname(__file__))
 PARENT_DIRECTORY = os.path.abspath(os.path.join(base_directory, os.pardir))
@@ -25,25 +27,20 @@ os.makedirs(RESULTS_DIR, exist_ok=True)
 os.makedirs(TABLES_DIR, exist_ok=True)
 os.makedirs(STRUCTURES_WITH_PREDICTED_ZN, exist_ok=True)
 
-KEEP_TEMP_TABLES = bool(os.getenv('KEEP_TEMP_TABLES'))
-
+# Load variables from the .env file into the environment
+load_dotenv()
+# KEEP_TEMP_TABLES = bool(os.getenv('KEEP_TEMP_TABLES'))
+KEEP_TEMP_TABLES = True
 def get_db_connection():
-    owner = True
-    try:
-        if owner == True:
             load_dotenv()  # Load environment variables from the .env file
-            user_id = os.getenv('DB_USER')
-            password = os.getenv('PASSWORD')
-            host = os.getenv('HOST')
+            # Retrieve the variables
+            user_id = os.getenv("DB_USER")
+            password = os.getenv("PASSWORD")
+            host = os.getenv("HOST")
             conn = psycopg2.connect(dbname="zincsight_pipeline_db", user=user_id, host=host, password=password)
-        else:
-            conn = psycopg2.connect(dbname="zincsight_pipeline_db", user="postgres", host="localhost")  # No password
 
-        return conn
+            return conn
 
-    except psycopg2.Error as e:
-        print(f"Error connecting to the database: {e}")
-        return None
 
 
 if __name__== "__main__":
